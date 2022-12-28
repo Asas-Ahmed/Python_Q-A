9df91e8654a6d5c76fb509b520102a28fed033dc
Asas <120548106+Asas-Ahmed@users.noreply.github.com>
2022-12-28 13:13:03 +0530

    Update 1.0

--- Answers/00030- Search Engine.py
+++ Answers/00030- Search Engine.py
@@ -1,8 +1,10 @@
 text = input()
 word = input()
+
 def search(text,word) :
     if word in text :
         return ('Word found')
     else:
         return('Word not found')
+    
 print(search(text,word))
